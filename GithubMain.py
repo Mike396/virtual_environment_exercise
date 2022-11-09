06bde16f9b1ecd480c593624b9d163811b5ed4c8
Mikael <mks@danskecommodities.com>
2022-11-09 14:13:05 +0100

    add setup.py

--- GithubMain.py
+++ GithubMain.py
@@ -1,10 +1,11 @@
 # import numpy as np
+def GithubMain():
 
-my_string = """ 
-This
-Is
-Sparta
-"""
+    my_string = """ 
+    This
+    Is
+    Sparta
+    """
+    return my_string
 
 # my_numbers = np.array([1, 2, 3, 4, 5])
-
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='GithubMain',
+
+    url='https://github.com/Mike396/virtual_environment_exercise.git',
+    author='mks',
+    author_email='',
+
+    py_modules=['GithubMain'],
+)
