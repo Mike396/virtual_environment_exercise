48f34d6a12b01ff7d8abc9c7fd3b37fba7bf1bc8
Mikael <mks@danskecommodities.com>
2022-11-10 16:22:45 +0100

    adjust setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 setup(
     name='GithubPackages',
@@ -7,5 +7,5 @@ setup(
     author='mks',
     author_email='',
 
-    packages=find_packages(),
+    packages=find_namespace_packages(),
 )
