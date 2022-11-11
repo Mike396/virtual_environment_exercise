2b70b7207318bf3cd8d55048fd10b6cd74499fca
Mikael <mks@danskecommodities.com>
2022-11-11 16:24:21 +0100

    adjust

---
+++ GithubPackages/__init__.py
@@ -0,0 +1,2 @@
+import GithubMain
+import GithubSecond
--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='GithubPackages',
-    version="1.0.0",
-    url='https://github.com/Mike396/virtual_environment_exercise.git',
-    packages=find_packages(),
+    version='1.0.0',
+    url='https://github.com/Mike396/virtual_environment_exercise',
+    author='mks',
+    packages=find_packages(), # ['GithubPackages', ]
 )
