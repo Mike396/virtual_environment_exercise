857ef088afa5e8e4d16586a114618d143890c3f8
Mikael <mks@danskecommodities.com>
2022-11-11 15:32:57 +0100

    adjust

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='GithubPackages',
     version="1.0.0",
     url='https://github.com/Mike396/virtual_environment_exercise.git',
-    packages=['GithubMain', 'GithubSecond'],
+    packages=find_packages(),
 )
