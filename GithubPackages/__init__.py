c7a26a05bb9b51b23c06b03d86bb3ff6be778c66
Mikael <mks@danskecommodities.com>
2022-11-14 11:17:10 +0100

    add functions to __init__.py

---
+++ GithubPackages/__init__.py
@@ -0,0 +1,16 @@
+
+def GithubMain():
+    my_string = """ 
+    This
+    Is
+    Sparta!!!
+    """
+    return my_string
+
+def GithubSecond():
+    my_string = """ 
+    This
+    Is
+    Athens!!!
+    """
+    return my_string
